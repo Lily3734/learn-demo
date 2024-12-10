f3de231cc8818a5e0d1bfa0aadc8b7d5d97b85d7
fate <fate@163.com>
2024-12-11 00:34:59 +0800

    push-MyThread

--- thread-app/src/com/itheima/d1_create_thread/MyThread.java
+++ thread-app/src/com/itheima/d1_create_thread/MyThread.java
@@ -6,6 +6,7 @@ public class MyThread extends Thread{
         //描述线程的的执行任务的
         for (int i = 0; i < 5; i++) {
             System.out.println("子线程MyThread输出："+i);
+            System.out.println("子线程MyThread输出："+i);
         }
     }
 }
